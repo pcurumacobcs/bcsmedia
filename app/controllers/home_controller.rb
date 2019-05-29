e2aadec3c3ec0d38fc5defdfef352f497ce7a096
Pablo Curumaco <pcurumaco@batrescs.com>
2019-05-29 15:27:34 -0600

    test api

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def myapi
+    render json: {'mensaje': 'Api funcionando'}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   get 'home/index'
 
+  get 'home/myapi'
+
   get '/', to: 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root "/", to: "home#index"
