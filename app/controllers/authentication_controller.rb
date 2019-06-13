6beabfaa9000278e6b69db12ceb57d3d734a1f17
Pablo Curumaco <pcurumaco@batrescs.com>
2019-06-13 08:36:01 -0600

    Login | Correccion and validate Username

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -2,6 +2,8 @@ class AuthenticationController < ApplicationController
   skip_before_action :verify_authenticity_token
   before_action :authorize_request, except: [:login, :register]
 
+  before_action :find_user_email, :only => [:login]
+
   # POST /auth/register
   def register
     @user = User.new(register_params)
@@ -20,18 +22,36 @@ class AuthenticationController < ApplicationController
 
   # POST /auth/login
   def login
-    @user = User.find_by_email(params[:email])
-    if @user.authenticate(params[:password])
+    # @user = User.find_by_email(params[:email])
+    if @user && @user.authenticate(params[:password])
       token = JsonWebToken.encode(user_id: @user.id)
       time = Time.now + 24.hours.to_i
       render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"),
                     username: @user.username }, status: :ok
     else
-      render json: { error: "unauthorized", status: 0 }, status: :unauthorized
+      render json: { messages: ["Invalid Email, Username or Password"], status: 0 }, status: :unauthorized
+      # render json: { error: "unauthorized", status: 0 }, status: :unauthorized
     end
   end
 
   private
+
+  def find_user_email
+    @user = User.find_by_email(params[:email])
+    if @user.nil?
+      @user = User.where(username: params[:username]).first
+      # render json: { hello: "hello" }
+    end
+  rescue ActiveRecord::RecordNotFound
+    resource_not_found
+    find_user_by_username
+  end
+
+  def find_user_by_username
+    @user = User.where(username: params[:username])
+  rescue ActiveRecord::RecordNotFound
+    resource_not_found
+  end
 
   def login_params
     params.permit(:email, :password)
