54a744ab22d04e53a4be1c41c3a3caab67e760ab
Pablo Curumaco <pcurumaco@batrescs.com>
2019-08-20 11:07:36 -0600

    Locations Seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,38 +7,68 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
 
-User.create(
+# make the first user
+User.create!(
   name: 'root',
   last_name: 'dev',
   username: 'root',
   email: 'admin@batrescs.com',
-  password_digest: '12345678',
-  status: '1'
+  password: '12345678',
+  status: 1
 )
 
-User.create(
-  name: 'eduardo',
-  last_name: 'llamas',
-  username: 'ellamas',
-  email: 'ellamas@batrescs.com',
-  password_digest: '12345678',
-  status: '1'
-)
-
-i = 0
-loop do
-  i += 1
-  Location.create(
-    name: Faker::Name.name,
-    address: 'Ciudad de Guatemala',
-    location_type_id: 1,
-    business_email: Faker::Internet.email,
-    lat: '0',
-    lng: '0',
-    status: 1
+# make screen brand
+5.times do |i|
+  ScreenBrand.create(
+    name: Faker::Company.name
   )
-  if i == 15
-    break       # this will cause execution to exit the loop
+end
+
+# make screen types
+2.times do |i|
+  ScreenType.create(
+    name: Faker::Name.name
+  )
+end
+
+orientations = [ "Horizontal", "Vertical" ]
+
+5.times do |i|
+  # make location type
+  location_type = LocationType.new(
+    name: Faker::Name.name,
+  )
+  location_type.save
+
+  10.times do |e|
+    # make locations
+    location = Location.new(
+      name: Faker::Name.name,
+      address: 'Ciudad de Guatemala',
+      location_type_id: location_type.id,
+      business_email: Faker::Internet.email,
+      lat: String(rand(14.5930298..14.648647)),
+      lng: String(rand(-90.5394762..-90.5158425)),
+      status: 1
+    )
+    location.save
+
+    rand(10).times do |s|
+      # make screens for location
+      screen = Screen.new(
+        screen_type_id: Integer(rand(1..5)),
+        screen_brand_id: Integer(rand(1..2)),
+        location_id: location.id,
+        code: "001",
+        model: "BCS",
+        sku: "BCS-01",
+        orientation: orientations[Integer(rand(0..1))],
+        size_inches: "15'",
+        description: "",
+        status: 1,
+      )
+      screen.save
+    end
   end
 end
 
